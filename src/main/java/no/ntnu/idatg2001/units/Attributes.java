61ca530482155111027aaca9a2850f7e6fe5c91b
Erik <erik@Eriks-MacBook-Pro.local>
2023-02-07 11:41:09 +0100

    Added units package, Unit motherclass and some functionality

---
+++ src/main/java/no/ntnu/idatg2001/units/Attributes.java
@@ -0,0 +1,68 @@
+package no.ntnu.idatg2001.units;
+
+public class Attributes {
+  // S.P.E.C.I.A.L stats
+  private int strength;
+  private int perception;
+  private int endurance;
+  private int charisma;
+  private int intelligence;
+  private int agility;
+  private int luck;
+
+  public int getStrength() {
+    return strength;
+  }
+
+  public void setStrength(int strength) {
+    this.strength = strength;
+  }
+
+  public int getPerception() {
+    return perception;
+  }
+
+  public void setPerception(int perception) {
+    this.perception = perception;
+  }
+
+  public int getEndurance() {
+    return endurance;
+  }
+
+  public void setEndurance(int endurance) {
+    this.endurance = endurance;
+  }
+
+  public int getCharisma() {
+    return charisma;
+  }
+
+  public void setCharisma(int charisma) {
+    this.charisma = charisma;
+  }
+
+  public int getIntelligence() {
+    return intelligence;
+  }
+
+  public void setIntelligence(int intelligence) {
+    this.intelligence = intelligence;
+  }
+
+  public int getAgility() {
+    return agility;
+  }
+
+  public void setAgility(int agility) {
+    this.agility = agility;
+  }
+
+  public int getLuck() {
+    return luck;
+  }
+
+  public void setLuck(int luck) {
+    this.luck = luck;
+  }
+}
---
+++ src/main/java/no/ntnu/idatg2001/units/Warrior.java
@@ -0,0 +1,4 @@
+package no.ntnu.idatg2001.units;
+
+public class Warrior {
+}
