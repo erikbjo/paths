9c5cb6a249255b3a1808bc324bf9aff31db8c537
Erik <erik@Eriks-MacBook-Pro.local>
2023-02-08 08:52:18 +0100

    Added items package with interfaces for diff. items

---
+++ src/main/java/no/ntnu/idatg2001/items/armor/Armor.java
@@ -0,0 +1,10 @@
+package no.ntnu.idatg2001.items.armor;
+
+import no.ntnu.idatg2001.items.BonusAttributes;
+import no.ntnu.idatg2001.items.Item;
+
+public interface Armor extends Item, BonusAttributes
+{
+
+  int armorSlot = 0;
+}
