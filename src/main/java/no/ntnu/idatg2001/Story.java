61bf0703cd35df8efd2f728b3db412ca4ff2dee7
eslatte <emilkle@stud.ntnu.no>
2023-02-08 11:22:14 +0100

    Added test classes for the most important classes and some javadoc

--- src/main/java/no/ntnu/idatg2001/Story.java
+++ src/main/java/no/ntnu/idatg2001/Story.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * The Story class is a container for the story. It contains the title of the story, a map of all
+ * the passages in the story, and the opening passage.
  *
  * @author Erik Bjørnsen and Emil Klevgård-Slåttsveen
  * @version 2023.02.02
@@ -14,36 +16,38 @@ public class Story {
     private Map<Link, Passage> passages;
     private Passage openingPassage;
 
-    /**
-     *
-     * @param title
-     * @param openingPassage
-     */
+
     public Story(String title, Passage openingPassage) {
         this.title = title;
         this.openingPassage = openingPassage;
         this.passages = new HashMap<>();
     }
 
+
     /**
+     * This function returns the title of the book
      *
-     * @return
+     * @return The title of the book.
      */
     public String getTitle() {
         return title;
     }
 
+
     /**
+     * This function returns the opening passage of the story.
      *
-     * @return
+     * @return The opening passage of the room.
      */
     public Passage getOpeningPassage() {
         return openingPassage;
     }
 
+
     /**
+     * This function adds a passage to the game
      *
-     * @param passage
+     * @param passage The passage that you want to add to the story.
      */
     public void addPassage(Passage passage) {
         this.openingPassage = passage;
--- src/main/java/no/ntnu/idatg2001/units/Player.java
+++ src/main/java/no/ntnu/idatg2001/units/Player.java
@@ -26,9 +26,13 @@ public class Player extends Unit {
 
   }
 
+
   /**
+   * If the health is greater than 0, add it to the current health and return true. Otherwise, return
+   * false
    *
-   * @param health
+   * @param health The amount of health to add to the player.
+   * @return A boolean value.
    */
   public boolean addHealth(int health) {
     if (health > 0) {
@@ -39,6 +43,12 @@ public class Player extends Unit {
     }
   }
 
+  /**
+   * If the mana is greater than 0, add it to the current mana and return true. Otherwise, return false
+   *
+   * @param mana The amount of mana to add to the player.
+   * @return A boolean value.
+   */
   public boolean addMana(int mana) {
     if (mana > 0) {
       super.setMana(super.getMana() + mana);
@@ -48,6 +58,13 @@ public class Player extends Unit {
     }
   }
 
+  /**
+   * If the energy is greater than 0, add it to the current energy and return true. Otherwise, return
+   * false
+   *
+   * @param energy The amount of energy to add to the robot.
+   * @return The boolean value of the if statement.
+   */
   public boolean addEnergy(int energy) {
     if (energy > 0) {
       super.setEnergy(super.getEnergy() + energy);
@@ -57,9 +74,13 @@ public class Player extends Unit {
     }
   }
 
+
   /**
+   * If the gold is greater than or equal to zero, add the gold to the player's gold, and return true.
+   * Otherwise, return false
    *
-   * @param gold
+   * @param gold The amount of gold to add to the player's gold.
+   * @return A boolean value.
    */
   public boolean addGold(int gold) {
     if (gold >= 0) {
