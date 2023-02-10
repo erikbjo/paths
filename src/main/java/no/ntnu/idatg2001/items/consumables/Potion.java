cf6d09b3736d3ae55bb02c7e0d6ed72ac6adc253
eslatte <emilkle@stud.ntnu.no>
2023-02-10 12:39:46 +0100

    Fixed constructores for the abstract classes Potion and Weapon

--- src/main/java/no/ntnu/idatg2001/items/consumables/Potion.java
+++ src/main/java/no/ntnu/idatg2001/items/consumables/Potion.java
@@ -3,7 +3,8 @@ package no.ntnu.idatg2001.items.consumables;
 import no.ntnu.idatg2001.items.Item;
 
 public abstract class Potion extends Item implements Consumable {
-  public Potion() {
-    super(name);
+  protected Potion(String name, int itemScore, int goldValue)
+  {
+    super(name, itemScore, goldValue);
   }
 }
