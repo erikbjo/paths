86c661172af6a85f175d206cf9ea1472614023ab
eslatte <emilkle@stud.ntnu.no>
2023-02-17 16:28:33 +0100

    Fetched changes in PlayerTest class from git repository

--- src/test/java/no/ntnu/idatg2001/units/PlayerTest.java
+++ src/test/java/no/ntnu/idatg2001/units/PlayerTest.java
@@ -10,36 +10,35 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class PlayerTest {
+public class PlayerTest
+{
+    Attributes testAttributes =
+        new Attributes(1,1,1,1,1,1,1);
+    Player testPlayer = new Player("Test",
+        5,5,5,5,5,testAttributes);
 
-  Attributes testAttributes = new Attributes(1,1,1,1,1,1,1);
-  Player testPlayer = new Player("Test", 5,5,5,5,5,testAttributes);
+    @Test
+    void dialog() {
+    }
 
+    @Test
+    void addHealth_mustEndInPositiveHealth() throws Exception {
+        testPlayer.setHealth(20);
+        testPlayer.addHealth(-19);
 
+        System.out.println(testPlayer.getHealth());
+        assertTrue(testPlayer.getHealth()>=0, "Yoyooyo");
+    }
 
+    @Test
+    public void testSetter_setsProperly() throws NoSuchFieldException, IllegalAccessException {
+        //Attributes testAttributes = new Attributes(1,1,1,1,1,1,1);
+        //Player testPlayer = new Player("Test", 5,5,5,5,5,testAttributes);
 
-  @Test
-  void dialog() {
-  }
+        testPlayer.setEnergy(5);
 
-  @Test
-  void addHealth_mustEndInPositiveHealth() throws Exception {
-    testPlayer.setHealth(20);
-    testPlayer.addHealth(-19);
-
-    System.out.println(testPlayer.getHealth());
-    assertTrue(testPlayer.getHealth()>=0, "Yoyooyo");
-  }
-
-  @Test
-  public void testSetter_setsProperly() throws NoSuchFieldException, IllegalAccessException {
-    //Attributes testAttributes = new Attributes(1,1,1,1,1,1,1);
-    //Player testPlayer = new Player("Test", 5,5,5,5,5,testAttributes);
-
-    testPlayer.setEnergy(5);
-
-    final Field field = testPlayer.getClass().getSuperclass().getDeclaredField("energy");
-    field.setAccessible(true);
-    assertEquals(5, field.get(testPlayer), "5");
-  }
+        final Field field = testPlayer.getClass().getSuperclass().getDeclaredField("energy");
+        field.setAccessible(true);
+        assertEquals(5, field.get(testPlayer), "5");
+    }
 }
