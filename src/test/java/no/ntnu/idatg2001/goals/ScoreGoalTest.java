4ff7e3a9e3ddc28635a0ce8af6f514a71b23cf1b
eslatte <emilkle@stud.ntnu.no>
2023-02-24 11:54:08 +0100

    Minor changes to some of the test classes

--- src/test/java/no/ntnu/idatg2001/goals/ScoreGoalTest.java
+++ src/test/java/no/ntnu/idatg2001/goals/ScoreGoalTest.java
@@ -4,30 +4,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import no.ntnu.idatg2001.units.Attributes;
 import no.ntnu.idatg2001.units.Player;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ScoreGoalTest
 {
 
+    Attributes attributes;
+    Player playerTrue;
+    Player playerFalse;
+    ScoreGoal scoreGoal;
+    @BeforeEach
+    void setUp(){
+     attributes = new Attributes(1,1,1,1,
+            1,1,1);
+     playerTrue = new Player("Test", 9,7,9,6,
+            12,attributes);
+     playerFalse = new Player("Test", -6,12,9,6,
+            12,attributes);
+     scoreGoal = new ScoreGoal(0);
+
+    }
     @Test
     void isScoreGoalFulfilled()
     {
-        Attributes attributes = new Attributes(1,1,1,1,
-            1,1,1);
-        ScoreGoal scoreGoal = new ScoreGoal(0);
-        Player player = new Player("Test", 9,7,9,6,
-            12,attributes);
-        assertTrue(scoreGoal.isFulfilled(player));
+        assertTrue(scoreGoal.isFulfilled(playerTrue));
     }
 
     @Test
     void scoreGoalIsNotFulfilled()
     {
-        Attributes attributes = new Attributes(1,1,1,1,
-            1,1,1);
-        ScoreGoal scoreGoal = new ScoreGoal(0);
-        Player player = new Player("Test", -6,12,9,6,
-            12,attributes);
-        assertFalse(scoreGoal.isFulfilled(player));
+        assertFalse(scoreGoal.isFulfilled(playerFalse));
+    }
+    @AfterEach
+    void tearDown(){
+
     }
 }
