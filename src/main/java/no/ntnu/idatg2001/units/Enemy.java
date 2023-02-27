bb149881f1abcd36c5b1c523b0b62106a533732c
Erik <erik@Eriks-MacBook-Pro.local>
2023-02-27 10:14:46 +0100

    Added enemy class and removed classes package

---
+++ src/main/java/no/ntnu/idatg2001/units/Enemy.java
@@ -0,0 +1,19 @@
+package no.ntnu.idatg2001.units;
+
+public class Enemy extends Unit {
+
+  public Enemy(String name, int score, int gold, int health, int mana, int energy, Attributes attributes) {
+    super.setName(name);
+    super.setScore(score);
+    super.setGold(gold);
+    super.setHealth(health);
+    super.setMana(mana);
+    super.setEnergy(energy);
+    super.setAttributes(attributes);
+  }
+
+  @Override
+  public void dialog() {
+
+  }
+}
