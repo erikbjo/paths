05d917f20c88111e9d2c0fc0d70c187afaea4eb3
Erik Bjørnsen <erbj@stud.ntnu.no>
2023-03-23 13:28:47 +0100

    Fixed PassageTest

--- src/test/java/no/ntnu/idatg2001/PassageTest.java
+++ src/test/java/no/ntnu/idatg2001/PassageTest.java
@@ -1,44 +1,43 @@
 package no.ntnu.idatg2001;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class PassageTest
-{
-    @Test
-    void addLinksShouldReturnTrue()
-    {
-        /**
-        Passage passage = new Passage("test","123");
-        List<Link> links = new ArrayList<>();
-        links.add(new Link("test1","1"));
-        links.add(new Link("test2","2"));
-        links.add(new Link("test3","3"));
-        assertTrue(links.size() >= 2,"Test success!");
-        assertTrue(passage.addLinks(new Link("True test", "123")));
-         */
+class PassageTest {
 
-        Passage passage = new Passage("test","123");
-        List<Link> links = new ArrayList<>();
-        links.add(new Link("test1","1"));
-        links.add(new Link("test2","2"));
-        boolean resultTrue = passage.addLinks(new Link("testTrue","3"));
-        assertTrue(resultTrue);
-    }
+  Passage passage;
+  List<Link> links;
 
-    @Test
-    void addLinksShouldReturnFalse()
-    {
-        Passage passage = new Passage("test","123");
-        List<Link> links = new ArrayList<>();
-        links.add(new Link("test4","4"));
-        boolean resultFalse = passage.addLinks(new Link("False test", "123"));
-        assertFalse(resultFalse);
-    }
+  @BeforeEach
+  void setUp() {
+    passage = new Passage("test", "123");
+    links = new ArrayList<>();
+    links.add(new Link("test1", "1"));
+    links.add(new Link("test2", "2"));
+  }
+
+  @AfterEach
+  void tearDown() {
+
+  }
+  @Test
+  void addLinksShouldReturnTrue() {
+    passage.setLinks(links);
+    boolean resultTrue = passage.addLinks(new Link("test3", "3"));
+    assertTrue(resultTrue);
+  }
+
+  @Test
+  void addLinksShouldReturnFalse() {
+    links.remove(0);
+    passage.setLinks(links);
+    boolean resultFalse = passage.addLinks(new Link("False test", "123"));
+    assertFalse(resultFalse);
+  }
 }
