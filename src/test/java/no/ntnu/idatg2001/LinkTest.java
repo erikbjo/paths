bb8108ce4d0f42252f50c28ea23fd63e108809a4
Emil <emilkle@stud.ntnu.no>
2023-03-30 12:48:27 +0200

    Added PathActions class to handle the different actions in the paths.

--- src/test/java/no/ntnu/idatg2001/LinkTest.java
+++ src/test/java/no/ntnu/idatg2001/LinkTest.java
@@ -1,29 +1,39 @@
 package no.ntnu.idatg2001;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LinkTest
 {
+    Link successLink;
+    Link failureLink;
+
+    @BeforeEach
+    void setUp(){
+        this.successLink = new Link("Success","1");
+        this.failureLink = new Link("Failure","1");
+    }
+
     @Test
     void testToStringPositive()
     {
-        Link link = new Link("Success","1");
         String expectedStringText1 = "Success";
-        assertEquals(expectedStringText1,expectedStringText1, link.toString());
+        assertEquals(expectedStringText1,expectedStringText1, successLink.toString());
     }
 
     @Test
     void testToStringNegative()
     {
-        Exception thrown = assertThrows(Exception.class, () -> {
-            Link link = new Link("Failure","1");
-            link.toString();
-            }, "The expected error was thrown");
-        assertEquals("Failure", thrown.getMessage());
+        assertNotEquals("Test", failureLink.toString());
     }
 
+    @AfterEach
+    void tearDown(){
+
+    }
 }
