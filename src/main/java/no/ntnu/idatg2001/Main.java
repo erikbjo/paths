118ffc46809b0138a0cf750c9f935d8ebaf4fca9
Erik Bjørnsen <erbj@stud.ntnu.no>
2023-03-30 13:43:00 +0200

    added ui package, start of gui and launcher

--- src/main/java/no/ntnu/idatg2001/Main.java
+++ src/main/java/no/ntnu/idatg2001/Main.java
@@ -2,6 +2,7 @@ package no.ntnu.idatg2001;
 
 import java.util.ArrayList;
 import java.util.List;
+import no.ntnu.idatg2001.ui.HomeView;
 
 public class Main {
   public static void main(String[] args) {
@@ -9,5 +10,7 @@ public class Main {
     List<Story> stories = new ArrayList<>();
     StoryFileHandler.writePathsFile(stories, "Stories.paths");
     // StoryFileHandler.readStoryFile("Stories.paths");
+
+    HomeView.mainApp(args);
   }
 }
---
+++ src/main/java/no/ntnu/idatg2001/ui/HomeView.java
@@ -0,0 +1,65 @@
+package no.ntnu.idatg2001.ui;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class HomeView extends Application {
+
+    public static void mainApp(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        AnchorPane anchorPane = new AnchorPane();
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10));
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        
+        VBox storiesVBox = new VBox();
+        VBox deadLinksVBox = new VBox();
+        HBox middleHBox = new HBox();
+
+        Text pathsGameText = new Text("Paths Game");
+        Text storiesText = new Text("Stories");
+        Text deadLinksText = new Text("Dead links");
+
+        TextArea storiesTextArea = new TextArea("storiesTextArea");
+        TextArea deadLinksTextArea = new TextArea("deadLinksTextArea");
+
+        storiesVBox.getChildren().add(storiesText);
+        storiesVBox.getChildren().add(storiesTextArea);
+
+        deadLinksVBox.getChildren().add(deadLinksText);
+        deadLinksVBox.getChildren().add(deadLinksTextArea);
+
+        middleHBox.getChildren().add(storiesVBox);
+        middleHBox.getChildren().add(deadLinksVBox);
+
+        Region fillerRegion = new Region();
+
+        Button startNewGameButton = new Button("Start new game");
+
+        gridPane.add(pathsGameText, 0,0);
+        gridPane.add(middleHBox,0,1);
+        gridPane.add(startNewGameButton,0,2);
+
+        anchorPane.getChildren().add(gridPane);
+        AnchorPane.setTopAnchor(gridPane, 10.0);
+        AnchorPane.setLeftAnchor(gridPane, 10.0);
+        AnchorPane.setRightAnchor(gridPane, 10.0);
+        AnchorPane.setBottomAnchor(gridPane, 10.0);
+
+        Scene scene = new Scene(anchorPane, 600, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
+
