6b143be9c6ada952f02460ed8cac48a26f5c796e
Emil <emilkle@stud.ntnu.no>
2023-03-30 14:36:35 +0200

    Added small changes to StoryFileHandler class

--- src/main/java/no/ntnu/idatg2001/PathActions.java
+++ src/main/java/no/ntnu/idatg2001/PathActions.java
@@ -1,13 +1,28 @@
 package no.ntnu.idatg2001;
 
-public class PathActions
-{
+public class PathActions {
     private String actionName;
 
     private String pathReference;
 
-    public PathActions(String actionName, String pathReference){
+    public PathActions(String actionName, String pathReference) {
         this.actionName = actionName;
+        this.pathReference = pathReference;
+    }
+
+    public String getActionName() {
+        return actionName;
+    }
+
+    public void setActionName(String actionName) {
+        this.actionName = actionName;
+    }
+
+    public String getPathReference() {
+        return pathReference;
+    }
+
+    public void setPathReference(String pathReference) {
         this.pathReference = pathReference;
     }
 }
