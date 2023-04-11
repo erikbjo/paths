2326cad0a2c93620bbfff8734622b549194f155c
Erik Bjørnsen <erbj@stud.ntnu.no>
2023-04-11 13:39:40 +0200

    added directory for music

--- src/main/java/no/ntnu/idatg2001/ui/views/HomeView.java
+++ src/main/java/no/ntnu/idatg2001/ui/views/HomeView.java
@@ -103,7 +103,7 @@ public class HomeView extends Application {
     Scene scene = new Scene(anchorPane, 600, 600);
     primaryStage.setScene(scene);
     primaryStage.show();
-    playMusic("src/main/resources/relaxing-145038.mp3");
+    playMusic("src/main/resources/music/relaxing-145038.mp3");
   }
 
   public void updateLanguage() {
