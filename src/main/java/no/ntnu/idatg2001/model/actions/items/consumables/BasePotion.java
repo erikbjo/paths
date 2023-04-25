afe82327d793d543a2730bdb8f14973a86f8091d
Erik Bjørnsen <erbj@stud.ntnu.no>
2023-04-25 13:20:32 +0200

    added javadoc and methods to goal classes

--- src/main/java/no/ntnu/idatg2001/model/actions/items/consumables/BasePotion.java
+++ src/main/java/no/ntnu/idatg2001/model/actions/items/consumables/BasePotion.java
@@ -2,6 +2,9 @@ package no.ntnu.idatg2001.model.actions.items.consumables;
 
 import no.ntnu.idatg2001.model.units.Player;
 
+/**
+ * The BasePotion class represents a potion that can be used by the player.
+ */
 public class BasePotion extends Potion {
 
   private final int energy;
@@ -9,13 +12,27 @@ public class BasePotion extends Potion {
   private int health;
   private int mana;
 
-  public BasePotion(String name, int itemScore, int goldValue ,int health, int mana, int energy) {
+  /**
+   * Constructor for the BasePotion class.
+   *
+   * @param name the name of the potion
+   * @param itemScore the score of the potion
+   * @param goldValue the gold value of the potion
+   * @param health the health the potion gives
+   * @param mana the mana the potion gives
+   * @param energy the energy the potion gives
+   */
+  public BasePotion(String name, int itemScore, int goldValue, int health, int mana, int energy) {
     super(name, itemScore, goldValue);
     this.health = health;
     this.mana = mana;
     this.energy = energy;
   }
-
+/**
+   * Use the potion.
+   *
+   * @param player the player that uses the potion
+   */
   @Override
   public void use(Player player) {
     player.addHealth(health);
