76d7697b93974e57ce009e677f432a0b8701ba1f
Erik Bjørnsen <erbj@stud.ntnu.no>
2023-05-01 11:14:35 +0200

    updated methods

--- src/main/java/no/ntnu/idatg2001/paths/ui/controllers/StoryViewController.java
+++ src/main/java/no/ntnu/idatg2001/paths/ui/controllers/StoryViewController.java
@@ -2,12 +2,25 @@ package no.ntnu.idatg2001.paths.ui.controllers;
 
 import java.util.List;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import no.ntnu.idatg2001.paths.model.Database;
 import no.ntnu.idatg2001.paths.model.Link;
 
 public class StoryViewController {
-  public void setLinksHBox(HBox linksHBox) {
+  private HBox linksHBox;
+  private Text storyHeadlineText;
+  private TextArea storyTextArea;
+
+  public StoryViewController(HBox linksHBox, Text storyHeadlineText, TextArea storyTextArea) {
+    this.linksHBox = linksHBox;
+    this.storyHeadlineText = storyHeadlineText;
+    this.storyTextArea = storyTextArea;
+  }
+
+  public void updateLinksHBox() {
+    linksHBox.getChildren().clear();
     List<Link> links =
         Database.getCurrentGame()
             .getStory()
@@ -15,8 +28,25 @@ public class StoryViewController {
     for (Link link : links) {
       Hyperlink hyperlink = new Hyperlink(link.getText());
       hyperlink.setOnAction(
-          event -> Database.getCurrentGame().go(link));
+          event -> {
+            Database.getCurrentGame().go(link);
+            updateStoryViewToNewPath();
+          });
       linksHBox.getChildren().add(hyperlink);
     }
+  }
+
+  public void setStoryHeadlineText() {
+    storyHeadlineText.setText(Database.getCurrentGame().getStory().getTitle());
+  }
+
+  public void setStoryTextArea() {
+    storyTextArea.setText(Database.getCurrentGame().getStory().getOpeningPassage().getContent());
+  }
+
+  private void updateStoryViewToNewPath() {
+    setStoryHeadlineText();
+    setStoryTextArea();
+    updateLinksHBox();
   }
 }
--- src/main/java/no/ntnu/idatg2001/paths/ui/views/StoryView.java
+++ src/main/java/no/ntnu/idatg2001/paths/ui/views/StoryView.java
@@ -3,10 +3,8 @@ package no.ntnu.idatg2001.paths.ui.views;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
@@ -14,17 +12,15 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import no.ntnu.idatg2001.paths.model.Database;
 import no.ntnu.idatg2001.paths.model.handlers.LanguageHandler;
 import no.ntnu.idatg2001.paths.ui.controllers.StoryViewController;
 import no.ntnu.idatg2001.paths.ui.standardObjects.StandardMenuBar;
 
 public class StoryView {
   private ResourceBundle resources;
+  private StoryViewController storyViewController;
   
   public void start(Stage stage) {
-    StoryViewController storyViewController = new StoryViewController();
-
     // Observes when the language in Database is changed, then calls updateLanguage()
     LanguageHandler.getObservableIntegerCounter()
         .addListener((obs, oldValue, newValue) -> updateLanguage());
@@ -34,6 +30,7 @@ public class StoryView {
         ResourceBundle.getBundle(
             "story", Locale.forLanguageTag(LanguageHandler.getCurrentLanguage().getLocalName()));
 
+    // Create a borderpane and a standard menubar
     BorderPane borderPane = new BorderPane();
     StandardMenuBar menuBar = new StandardMenuBar();
     borderPane.setTop(menuBar);
@@ -44,21 +41,29 @@ public class StoryView {
     rootVBox.setPadding(new Insets(50));
     rootVBox.setAlignment(Pos.CENTER); // Center the elements vertically and horizontally
 
-
+    // Create a headline and a text area for the story
     Text storyHeadlineText = new Text("storyHeadlineText");
     TextArea storyTextArea = new TextArea("storyTextArea");
     storyTextArea.setEditable(false);
 
+    // HBox for the hyperlinks
     HBox linksHBox = new HBox(100);
     linksHBox.setAlignment(Pos.CENTER);
-    storyViewController.setLinksHBox(linksHBox);
 
+    // Add all the elements to the root VBox
     rootVBox.getChildren().add(storyHeadlineText);
     rootVBox.getChildren().add(storyTextArea);
     rootVBox.getChildren().add(linksHBox);
 
+    // Add borderpane and rootVBox to rootAnchorPane
     rootAnchorPane.getChildren().add(borderPane);
     rootAnchorPane.getChildren().add(rootVBox);
+
+    // Initialize the controller with the dynamic elements
+    storyViewController = new StoryViewController(linksHBox, storyHeadlineText, storyTextArea);
+    storyViewController.updateLinksHBox();
+    storyViewController.setStoryHeadlineText();
+    storyViewController.setStoryTextArea();
 
     Scene scene = new Scene(rootAnchorPane, 600, 600);
     scene.getStylesheets().add("cssfiles/storyStyle.css");
