4959184262309f88e25e41f56f2781685f578ae0
Erik Bjørnsen <erbj@stud.ntnu.no>
2023-05-04 20:46:27 +0200

    added newgameview and controller

---
+++ src/main/java/no/ntnu/idatg2001/paths/ui/views/NewGameView.java
@@ -0,0 +1,27 @@
+package no.ntnu.idatg2001.paths.ui.views;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import no.ntnu.idatg2001.paths.ui.standardObjects.StandardMenuBar;
+
+public class NewGameView {
+
+  private final Stage primaryStage;
+
+  public NewGameView(Stage primaryStage) {
+    this.primaryStage = primaryStage;
+    primaryStage.setTitle("New Game");
+
+    // Create a borderpane and a standard menubar
+    BorderPane root = new BorderPane();
+    StandardMenuBar menuBar = new StandardMenuBar(primaryStage);
+    root.setTop(menuBar);
+    AnchorPane rootAnchorPane = new AnchorPane();
+
+    Scene scene = new Scene(root, 600, 600);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+}
